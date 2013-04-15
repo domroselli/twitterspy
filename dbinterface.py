5fbaff75f9a32d62a886b98b1ef6dcfff6e1c87f
Dom Roselli <eazysneazy@gmail.com>
2013-04-15 18:26:36 -0400

    added find_unknown_user_ids and tests

--- dbinterface.py
+++ dbinterface.py
@@ -3,10 +3,20 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import relationship, backref
 from email.utils import parsedate_tz
 from datetime import datetime, timedelta
+from dbentities import User
 
 def db_session_factory(Base, engine_source, session_maker, echo_on):
     engine = create_engine(engine_source, echo=echo_on)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
+
     return Session()
 
+def find_unknown_user_ids(session, user_ids):
+    result = session.query(User.user_id)\
+                 .filter(User.user_id.in_(user_ids)).all()
+    unknown_users = set(user_ids)
+    known_users = set(zip(*result)[0]) if result else set()
+    unknown_users = unknown_users - known_users
+
+    return list(unknown_users)
--- tests/test_dbinterface.py
+++ tests/test_dbinterface.py
@@ -8,3 +8,13 @@ def test_db_session_factory():
     session = db_session_factory(Base, engine_source, sessionmaker, echo_on)
     result = session.execute('SELECT 1').first()
     assert result[0] == 1
+
+def test_find_unknown_user_ids():
+    user_ids = [1,2,3]
+    engine_source = 'sqlite:///:memory:'
+    echo_on = False
+    session = db_session_factory(Base, engine_source, sessionmaker, echo_on)
+    unknown_user_ids = find_unknown_user_ids(session, user_ids)
+
+    assert  set(user_ids) == set(unknown_user_ids)
+
